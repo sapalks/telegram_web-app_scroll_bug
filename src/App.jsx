5cea2bb773d3f3aa80ce7ac331df38befc7b70ed
Sapunkov Alexey <sapalks@gmail.com>
2023-03-21 15:16:06 +0400

    rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { useCallback } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useCallback } from "react";
+import React from "react";
 
 function App() {
   const arr = new Array(200).fill(<div>1</div>);
-  const onClick=useCallback(()=>{
-    const app = document.getElementsByClassName('App')[0];
-    if(app.classList.contains('bad-scroll')){
-      app.classList.remove('bad-scroll')
-    }else{
-      app.classList.add('bad-scroll')
+  const onClick = useCallback(() => {
+    const app = document.getElementsByClassName("App")[0];
+    if (app.classList.contains("bad-scroll")) {
+      app.classList.remove("bad-scroll");
+    } else {
+      app.classList.add("bad-scroll");
     }
-  },[])
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={onClick}>Change scroll mode</button>
-        <img src={logo} className="App-logo" alt="logo"  />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
