e55f7c8c0264bf7ab082de7190ee87a250368313
Sapunkov Alexey <sapalks@gmail.com>
2023-03-21 14:11:08 +0400

    implement incorrect work situation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback } from 'react';
 
 function App() {
-  const arr = new Array(200).fill(<div>1</div>)
+  const arr = new Array(200).fill(<div>1</div>);
+  const onClick=useCallback(()=>{
+    const app = document.getElementsByClassName('App')[0];
+    if(app.classList.contains('bad-scroll')){
+      app.classList.remove('bad-scroll')
+    }else{
+      app.classList.add('bad-scroll')
+    }
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={onClick}>Change scroll mode</button>
+        <img src={logo} className="App-logo" alt="logo"  />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
